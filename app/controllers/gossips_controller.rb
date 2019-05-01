4835cbd4d2560861be12107b70bcb3ce81cdf79b
Mutmut <briend.mathilde@gmail.com>
2019-05-01 19:13:45 +0200

    Suppression des potins

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -9,18 +9,43 @@ class GossipsController < ApplicationController
   end
 
   def create
-
     @gossip = Gossip.new(title:params[:title], content:params[:content], user_id:170) # avec xxx qui sont les données obtenues à partir du formulaire
 
     if @gossip.save # essaie de sauvegarder en base @gossip
       redirect_to '/'
-
       # si ça marche, il redirige vers la page d'index du site
     else
       # sinon, il render la view new (qui est celle sur laquelle on est déjà)
       render 'new'
     end
 
+  end
+
+  def edit
+    @gossip = Gossip.find(params[:id])
+  end
+
+  def update
+    @gossip = Gossip.find(params[:id])
+
+    if @gossip.update(gossip_params)
+      redirect_to @gossip
+    else
+      render :edit
+    end
+
+  end
+
+  def destroy
+    @gossip = Gossip.find(params[:id])
+    @gossip.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def gossip_params
+    params.require(:gossip).permit(:title, :content)
   end
 
 end
