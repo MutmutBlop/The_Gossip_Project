eb0b6d14edcc129312060abd85c71f1808266652
Mutmut <briend.mathilde@gmail.com>
2019-05-02 21:20:58 +0200

    Avec les liiikes

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   resources :users
   resources :cities
   resources :sessions, only: [:new, :create, :destroy]
+  resources :likes, only: [:show, :new, :create, :destroy]
 
   get '/team', to: 'static_pages#team'
   get '/contact', to: 'static_pages#contact'
